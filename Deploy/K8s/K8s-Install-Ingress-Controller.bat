cf9e27361cbdd9652edcea376585952804b42406
Xavier JEUNOT <xavier.jeunot@gfi.fr>
2020-01-07 09:17:33 +0100

    Migration > Kubernetes 1.17 > Helm 3

--- Deploy/K8s/K8s-Install-Ingress-Controller.bat
+++ Deploy/K8s/K8s-Install-Ingress-Controller.bat
@@ -3,14 +3,8 @@ REM ---------------------------------------------------------------------
 REM ------------------------- MyPhysicalStore ---------------------------
 REM ---------------------------------------------------------------------
 REM ----- Manage Install Ingress Controller into Kubernetes Cluster -----
-REM ----- Version				2019-06-19						---------
+REM ----- Version				2019-12-03						---------
 REM ---------------------------------------------------------------------
-
-rem Verify Helm/Tiller Installation.
-IF NOT EXIST %UserProfile%/.helm (
-	ECHO Error : No Helm/Tiller Install Detected !
-	GOTO END
-)
 
 rem Install.
 if "%~1"=="--install" (
@@ -22,7 +16,7 @@ if "%~1"=="--install" (
 
 	rem Install Nginx Ingress.
 	ECHO "#################### Install Nginx Ingress ####################"
-	helm install nginx-stable/nginx-ingress --name psa-ingress-controller --set controller.service.externalIPs="{192.168.137.21,192.168.137.22}"
+	helm install psa-ingress-controller nginx-stable/nginx-ingress --set controller.service.externalIPs="{192.168.137.22,192.168.137.23}"
 
 	GOTO END
 )
@@ -32,7 +26,7 @@ if "%~1"=="--remove" (
 
 	rem Remove Nginx Ingress.
 	ECHO "#################### Remove Nginx Ingress ####################"
-	helm del --purge psa-ingress-controller
+	helm uninstall psa-ingress-controller
 
 	GOTO END
 )
